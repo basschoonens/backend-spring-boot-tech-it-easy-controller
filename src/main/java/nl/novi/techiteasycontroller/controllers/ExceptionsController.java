4ff5c1889e85cf94aa1a55b8a9b66a6731a02d9b
basschoonens <bas.schoonens@gmail.com>
2024-02-21 13:19:19 +0100

    MethodArgumentNotValidException bijgewerkt van Mark ontvangen

--- src/main/java/nl/novi/techiteasycontroller/controllers/ExceptionsController.java
+++ src/main/java/nl/novi/techiteasycontroller/controllers/ExceptionsController.java
@@ -4,9 +4,13 @@ import nl.novi.techiteasycontroller.exceptions.RecordNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionsController {
@@ -24,9 +28,21 @@ public class ExceptionsController {
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<?> exception(MethodArgumentNotValidException exception) {
+        BindingResult result = exception.getBindingResult(); // De BindingResult van een exception, bevat alle errors
+        return ResponseEntity.badRequest().body(processBindingResult(result));
+    }
 
-    public ResponseEntity<Object> exception(MethodArgumentNotValidException exception) {
-        return new ResponseEntity<>("Vul correcte waardes in.", HttpStatus.BAD_REQUEST);
+    private String processBindingResult(BindingResult bindingResult) {
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();// Een FieldError is de error, daaruit gaan we de errorMessage halen om aan de gebruiker te tonen
+        StringBuilder stringBuilder = new StringBuilder(); // Met een StringBuilder kun je makkelijk een String bouwen.
+        for (FieldError fieldError: fieldErrors) {
+            stringBuilder.append(fieldError.getField()); // Voeg de naam van het overtredende veld toe aan de String (zoals "name")
+            stringBuilder.append(" : "); // Voeg een : toe aan de String
+            stringBuilder.append(fieldError.getDefaultMessage()); // Voeg de message van de error toe aan de String (zoals "cannot be null")
+            stringBuilder.append("\n"); // Voeg een enter toe, zodat elke error op een nieuwe regel staat.
+        }
+        return stringBuilder.toString();
     }
 
 }
